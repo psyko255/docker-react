2771294362e35500160ba6fa984b0fcdd7d3fb00
Edward Psyk <edpsyk@Eds-MacBook-Pro.local>
2019-07-18 19:39:59 -0400

    changed feature branch

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi mom
+          Changed on feature branch
         </a>
       </header>
     </div>
